ced1985172994c74e0396117afff59e754939d8b
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-11-28 10:33:42 +0200

    update URL

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  redirects: [
-    {
-      source: "https://www.andriinepomniashchyi.pp.ua/(.*)",
-      destination: "https://andriinepomniashchyi.pp.ua/$1",
-      permanent: true,
-    },
-  ],
 };
 
 module.exports = nextConfig;
