eead66fcd6d0c1651ac6451f6496d764d82879ea
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-12-03 12:30:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   trailingSlash: true,
+  pageExtensions: ["api.js"],
   webpack(config) {
     // if (isServer) {
     //   require("./scripts/generate-sitemap");
