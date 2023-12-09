5f8bf8c0c1df748ff52f433b58c5faf926919fdd
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-12-09 21:26:36 +0200

    add logo in navigation

--- src/components/Monogram/Monogram.js
+++ src/components/Monogram/Monogram.js
@@ -1,18 +1,17 @@
 import { forwardRef, useId } from "react";
 
-import { classes } from "utils/style";
 import styles from "./_Monogram.module.scss";
 
-// export const Monogram = forwardRef(
-//   ({ highlight, className, ...props }, ref) => {
-//     const id = useId();
-//     const clipId = `${id}monogram-clip`;
-//         return (
+import Logo from "./logo.svg";
 
-//     );
-//   }
-// );
-
-export const Monogram = () => {
-  return <div>Hello Nonna I Love You</div>;
-};
+export const Monogram = forwardRef(
+  ({ highlight, className, ...props }, ref) => {
+    const id = useId();
+    const clipId = `${id}monogram-clip`;
+    return (
+      <div className={styles.logo}>
+        <Logo id={clipId} ref={ref} {...props} />
+      </div>
+    );
+  }
+);
